e9b6b2b086bc83216fb8185952362de8f9bae077
rpf13 <rpf13@me.com>
2023-03-22 06:08:52 +0000

    Add main view and template

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+# First we need to import our views from the current directory
+from . import views
+from django.urls import path
+
+
+# Creating the view with a blank path (empty string), means its our default
+# Since we use class based views, we need to add the .as_view method
+# at the end of it.
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+# We have to import the model and the related class
+# for which we want to create a view.
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    # we only display the published post (status = 1, 0 = draft) and
+    # we sort in descending order
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # Template name, which our view will render
+    template_name = 'index.html'
+    # The generic ListView includes a default set of posts visible on
+    # one page, called "pagination". We set it here to 6 posts. If there
+    # is more, Django will create autom. a page navigation.
+    paginate_by = 6
