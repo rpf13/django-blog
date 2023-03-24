2f94989b601ff802b54dca178ae2c9fbcd8d996a
rpf13 <rpf13@me.com>
2023-03-24 15:32:25 +0000

    Set debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # because of CORS security setting, we must add below line, otherwise
 # the comments section would not be available anymore after deployment.
